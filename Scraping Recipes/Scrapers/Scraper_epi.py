5d43f25260839d7dafc66472e6138b93dedbda7b
ankitgoyal0301 <goyalankit3129@gmail.com>
2020-09-06 02:41:09 +0530

    epicurious scrapper added. Approx. 36k recipes.

--- Scraping Recipes/Scrapers/Scraper_epi.py
+++ Scraping Recipes/Scrapers/Scraper_epi.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup as soup
 import pandas as pd
 import numpy as np
 
-base_url = 'https://www.allrecipes.com/recipes/?page='
+base_url = 'https://www.epicurious.com/search?content=recipe&page='
+prefix_url = 'https://www.epicurious.com'
 
 recipe_titles = []
 recipe_img_links = []
@@ -14,7 +15,7 @@ recipe_intructions = []
 
 recipe_count = 1
 
-for i in range(2,4):# 417):
+for i in range(1,3):# 2032):
     
     print("Page "+str(i)+" scraping started!!!")
     
@@ -25,10 +26,12 @@ for i in range(2,4):# 417):
     
     recipe_links_on_page = []
     
-    anchors = page_soup.find_all('a', {'class': 'recipeCard__imageLink', 'href': True})
-
-    for anchor in anchors:
-        recipe_links_on_page.append(anchor['href'])
+    recipe_links = page_soup.find_all('h4', {'class': 'hed'})
+    
+    for link in recipe_links:
+        recipe_links_on_page.append(prefix_url + link.find('a')['href'])
+        
+    
         
     for recipe_url in recipe_links_on_page:
         req_recipe = Request(recipe_url, headers={'User-Agent':'Mozilla/5.0'})
@@ -36,11 +39,13 @@ for i in range(2,4):# 417):
         page_soup_recipe = soup(webpage_recipe,"html.parser")
         
         # Name of Recipe
-        name = page_soup_recipe.find('h1',class_='headline heading-content').text
+        name = page_soup_recipe.find('h1',itemprop="name").text
         recipe_titles.append(name)
         
+        
         # Ingredients
-        ingredients = page_soup_recipe.find_all('span',class_='ingredients-item-name')
+        ingredients = page_soup_recipe.find('div',class_='ingredients-info').find('ol', class_='ingredient-groups').find_all('li', {'class':'ingredient'})
+        
         temp_ingredients = []
         
         for ingredient in ingredients:
@@ -48,30 +53,26 @@ for i in range(2,4):# 417):
         
         recipe_ingredients.append(temp_ingredients)
         
-        # Recipe 
-        recipes = page_soup_recipe.find("ul", { "class" : "instructions-section" }).findAll("li", recursive=False)
+        # Recipe
+        recipes = page_soup_recipe.find('div',class_='instructions').find('ol', class_='preparation-groups').find_all('li', {'class':'preparation-step'})
+        
         temp_recipe = []
         
-        for i in range(len(recipes)):
-            temp_recipe.append(recipes[i].find('p').text)
+        for recipe in recipes:
+            temp_recipe.append(recipe.text.strip())
         
         recipe_intructions.append(temp_recipe)
     
-        # Image
-        temp_img = page_soup_recipe.find("div", { "class" : "docked-sharebar-content-container" })
-        temp_img = temp_img.find("div",{"class" : "image-container"})
-        temp_img = temp_img.find("div")
+        # Image 
+        temp_img = page_soup_recipe.find('div',class_='social-img').find('img')['srcset']
+        recipe_img_links.append(temp_img)
         
-        recipe_img_links.append(temp_img["data-src"])
-        
-        # Done with the recipe
         print("Recipe "+str(recipe_count)+" Scrapped")
         recipe_count += 1
         
     print("Page "+str(i)+" Scraped!!!")
-         
-# Storing Dataframe to CSV
+        
 dataset_ar = pd.DataFrame(list(zip(recipe_titles, recipe_ingredients, recipe_intructions, recipe_img_links)), 
                columns =['Recipe_name', 'Recipe_ingredients', 'Recipe_instructions', 'Recipe_img_link'])
 
-dataset_ar.to_csv('./Datasets/dataset_ar.csv')
+dataset_ar.to_csv('./Datasets/dataset_epi.csv')
