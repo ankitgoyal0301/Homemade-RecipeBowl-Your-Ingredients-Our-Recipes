f4ad567ae30d380ff9c4099a61e8d282c7f8cf08
ankitgoyal0301 <goyalankit3129@gmail.com>
2020-11-03 00:54:15 +0530

    About us page added

--- Website Front End/src/Login/src/about.js
+++ Website Front End/src/Login/src/about.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React , {useState} from "react";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import "./App.scss";
 import Navbar from "./components/Navbar";
 import "./components/Navbar.css";
 import { FooterContainer } from './containers/footer';
+import Section1 from "./components/about/section-1";
+import Features from "./components/about/features";
+import Info from "./components/about/info";
+import Team from "./components/about/team";
+import "./about.css"
+import JsonData from './data/data.json';
 
 // function App() {
   //   return (
@@ -14,6 +20,8 @@ import { FooterContainer } from './containers/footer';
     // }
 
 function DashBoard(){
+  const [data, setData] = useState({landingPageData : JsonData});
+
     return (
       <div>
         <Router>
@@ -21,8 +29,20 @@ function DashBoard(){
             <Navbar/>
           </div>
 
-          <div style={{marginBottom:"700px"}}>
+          <div>
+            <Section1/>
+          </div>
 
+          <div>
+            <Features/>
+          </div>
+
+          <div>
+            <Info/>
+          </div>
+
+          <div>
+            <Team/>
           </div>
 
           <div>
